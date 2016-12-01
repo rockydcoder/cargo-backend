5b42cba86cbd9af154db85e78f8924c532a38c99
ppatra <ppatra@paypal.com>
2016-12-01 11:33:44 +0530

    Added elixir buildpack config file

--- config/config.exs
+++ config/config.exs
@@ -2,26 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# config :cargo, Cargo.Repo.Activities,
-#   adapter: Ecto.Adapters.Postgres,
-#   database: "cargo_repo",
-#   hostname: "localhost"
-#
-# config :cargo, Cargo.Repo.Loyalties,
-#   adapter: Ecto.Adapters.Postgres,
-#   database: "cargo_repo",
-#   hostname: "localhost"
-#
-# config :cargo, Cargo.Repo.Vehicles,
-#   adapter: Ecto.Adapters.Postgres,
-#   database: "cargo_repo",
-#   hostname: "localhost"
-#
-# config :cargo, Cargo.Repo.Partners,
-#   adapter: Ecto.Adapters.Postgres,
-#   database: "cargo_repo",
-#   hostname: "localhost"
-
 config :cargo, Cargo.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "cargo_repo",
